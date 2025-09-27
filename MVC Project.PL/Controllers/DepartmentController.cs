7bec2507ccc43fe3e70abe9d1305932e421790eb
Mono1127 <mohandwael27@gmail.com>
2025-09-27 03:18:55 +0300

    All Sec 3

---
+++ MVC Project.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.BLL.Interfaces;
+using MVC_Project.BLL.Repositories;
+using MVC_Project.DAl.Models;
+using MVC_Project.PL.Dtos;
+
+namespace MVC_Project.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _repository;
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _repository =  departmentRepository;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            var departments = _repository.GetAll();
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+           if( ModelState.IsValid)
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+               var count =  _repository.Add(department);
+                if (count > 0) 
+                {
+                 return RedirectToAction(nameof(Index));
+                }
+            }
+           return View(model);
+        }
+
+
+
+
+    }
+}
---
+++ MVC Project.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is Required ")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is Required ")]
+        public string Name { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
